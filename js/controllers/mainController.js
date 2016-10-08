73bdd44beb77b047e1049edd5255b70ea5719e4d
uxpaul <hoedevproject@gmail.com>
2016-10-08 14:21:59 +0200

    Formulaire + AngularJs

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,4 +1,25 @@
 // MAIN CONTROLLER
 function mainController($scope) {
-    
-}
+    $scope.title = "Tableau AngularJS";
+    $scope.students = [
+      {
+        "nom":"Paul",
+        "prenom":"DE BARTHEZ",
+        "email":"ipolomac@me.com"
+      }
+    ]
+
+
+    $scope.add = function() {
+        var data = {};
+        data.nom = $scope.nom;
+        data.prenom = $scope.prenom;
+        data.email = $scope.email;
+
+        $scope.students.push(data);
+
+        $scope.nom = "";
+        $scope.prenom ="";
+        $scope.email ="";
+    }
+};
